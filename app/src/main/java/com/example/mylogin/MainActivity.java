ae746d801068c827188c33f3564b2ca9451dd79b
unknown <rzkykaligiss@gmail.com>
2023-09-24 03:04:01 +0800

    TUGAS1

---
+++ app/src/main/java/com/example/mylogin/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mylogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import kotlin.text.UStringsKt;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword ;
+    private Button btnLogin;
+
+    private String username = "Rizky";
+    private String password = "ADMIN";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etUsername.getText().toString().equalsIgnoreCase(username) && etPassword.getText().toString().equalsIgnoreCase(password)) {
+                    Intent login = new Intent(MainActivity.this, dashboard.class);
+                    startActivity(login);
+
+                    Toast.makeText(MainActivity.this, "LOGIN BERHASIL!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Username atau Password yang anda masukkan salah.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
